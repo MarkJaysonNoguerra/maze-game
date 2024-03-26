c2d85e32b574283b8a9633eb78429063f9a3ba43
mark <noguerra9999@gmail.com>
2024-03-27 02:09:26 +0800

    Updates

--- dist/walls.js
+++
@@ -1,7 +0,0 @@
-export var Wall;
-(function(Wall2) {
-  Wall2[Wall2["Top"] = 0] = "Top";
-  Wall2[Wall2["Right"] = 1] = "Right";
-  Wall2[Wall2["Bottom"] = 2] = "Bottom";
-  Wall2[Wall2["Left"] = 3] = "Left";
-})(Wall || (Wall = {}));
